464b0708edb126b9a041e068ee54d18d2814cde0
Maelle AHOUMENOU <senma94@gmail.com>
2018-10-22 21:55:54 +0100

    create fedapay ruby library

---
+++ lib/fedapay/ruby.rb
@@ -0,0 +1,7 @@
+require "fedapay/ruby/version"
+
+module Fedapay
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/fedapay/ruby/version.rb
@@ -0,0 +1,5 @@
+module Fedapay
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end

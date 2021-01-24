614024430a1a1acafb47ab80fc3fea4858cfcb64
Boris Koumondji <brexis2009@yahoo.fr>
2021-01-24 21:00:14 +0000

    Fix customer delete_transaction

--- lib/fedapay/customer.rb
+++ lib/fedapay/customer.rb
@@ -8,11 +8,5 @@ module FedaPay
     extend FedaPay::APIOperations::List
 
     OBJECT_NAME = 'customer'.freeze
-
-    # The API request for deleting a card or bank account and for detaching a
-    # source object are the same.
-    class << self
-      alias detach_transaction delete_transaction
-    end
   end
 end
